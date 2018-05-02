4ed1ffb572efacc5a5dfe9139ddfdcd72c4d6ad3
HVLLOwe3n <roman.synovets@gmail.com>
2018-05-02 16:00:40 +0300

    heroku config 2/n

--- SOCIAL_ROOT/settings.py
+++ SOCIAL_ROOT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'gif=b5hdn*u90f%h)b*e%qai21b21pvgl+3h4pm=6fvs&urno0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []  # '.herokuapp.com', '127.0.0.1'
 
 
 # Application definition
