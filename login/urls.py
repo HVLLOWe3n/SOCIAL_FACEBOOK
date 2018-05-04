dc26ebefebd8f1d657e11994ac9e161b42f52e23
HVLLOwe3n <roman.synovets@gmail.com>
2018-05-04 20:01:06 +0300

    heroku config 6/n

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from login import views
 
 urlpatterns = [
-    path('login/', views.login_function, name='login')
+    path('login/', views.login_function, name='login'),
+    path('login_success/', views.get_login_success, name='success')
 ]
--- login/views.py
+++ login/views.py
@@ -4,3 +4,14 @@ import requests
 
 def login_function(request):
     return render(request, 'login/login.html')
+
+
+def get_login_success(request):
+    code = request.GET.get('code', None)
+    at = requests.get('https://graph.facebook.com/v3.0/oauth/access_token?client_id=421333258293431&redirect_uri=https://socialfacebook.herokuapp.com/login/&client_secret=bf293ff8dc4d986f79da7f36585b26d7&code={}'.format(code)).json()
+
+    context = {
+        'at_json': at
+    }
+
+    return render(request, 'login/facebook_login_success.html', context)
