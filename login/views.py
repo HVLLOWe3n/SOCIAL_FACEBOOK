05e43f0b4552b3132c97b7d1496d658db78fa228
HVLLOwe3n <roman.synovets@gmail.com>
2018-05-05 10:23:46 +0300

    facebook access_token task

--- login/views.py
+++ login/views.py
@@ -9,7 +9,7 @@ def login_function(request):
 def get_login_success(request):
     code = request.GET.get('code', None)
     original_url = 'https://www.facebook.com/v3.0/dialog/oauth?client_id=421333258293431&redirect_uri=https://socialfacebook.herokuapp.com/login_success/&state=manage_pages'
-    at = requests.get('https://graph.facebook.com/v3.0/oauth/access_token?client_id=421333258293431&redirect_uri={}&client_secret=bf293ff8dc4d986f79da7f36585b26d7&code={}'.format(original_url, code))
+    at = requests.get('https://graph.facebook.com/v3.0/oauth/access_token?client_id=421333258293431&redirect_uri={}&client_secret=bf293ff8dc4d986f79da7f36585b26d7&code={}'.format(original_url, code)).json()
 
     context = {
         'at_json': at
