56efaf0c2d4b238a0dbd88e7958ea31cce060bb4
HVLLOwe3n <roman.synovets@gmail.com>
2018-05-05 10:09:39 +0300

    heroku config 9/n

--- login/views.py
+++ login/views.py
@@ -8,7 +8,7 @@ def login_function(request):
 
 def get_login_success(request):
     code = request.GET.get('code', None)
-    at = requests.get('https://graph.facebook.com/v3.0/oauth/access_token?client_id=421333258293431&redirect_uri=https://socialfacebook.herokuapp.com/login_success/&client_secret=bf293ff8dc4d986f79da7f36585b26d7&code={}'.format(code)).json()
+    at = requests.get('https://graph.facebook.com/v3.0/oauth/access_token/?client_id=421333258293431&redirect_uri=https://socialfacebook.herokuapp.com/login_success/&client_secret=bf293ff8dc4d986f79da7f36585b26d7&code={}'.format(code)).json()
 
     context = {
         'at_json': at
